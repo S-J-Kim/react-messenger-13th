a6ab03e9a4437fb6f7866b823b48fa64b18f7751
SJKim <hedro0458@gmail.com>
2021-05-08 17:11:44 +0900

    chore: 필요없는 코드들 삭제

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './chatRoom/App';
-import FriendsList from './friendList/FriendsList.js';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ChatApp from './base/ChatApp';
 
 ReactDOM.render(
